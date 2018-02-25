f540b97da89bff4d16e8415b4dc61f65ea9a7c45
ecgeoffroy <36522383+ecgeoffroy@users.noreply.github.com>
2018-02-25 16:21:41 -0600

    Add files via upload
    Added random R, G, B values for each ring to compute a random color for each ring. Can someone figure out how to get it animated-ish?

--- OlympicRingView.java
+++ OlympicRingView.java
@@ -1,16 +1,18 @@
+import java.awt.Container;
+import java.awt.*;
 import javax.swing.JFrame;
 public class OlympicRingView {
 	public static void main(String[] args)
 	   {
 	      JFrame frame = new JFrame();
+	      
+	      final int FRAME_WIDTH = 200;
+	      final int FRAME_HEIGHT = 150;
 
-	      final int FRAME_WIDTH = 300;
-	      final int FRAME_HEIGHT = 230;
-
-	      frame.setSize(300, 230);
+	      frame.setSize(200, 150);
 	      frame.setTitle("OlympicRingViewer");
 	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+	      
 	      P1egeoffroyOlympicRings component = new P1egeoffroyOlympicRings();
 	      frame.add(component);
 
--- P1egeoffroyOlympicRings.java
+++ P1egeoffroyOlympicRings.java
@@ -2,27 +2,67 @@ import javax.swing.JComponent;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Color;
-
+import java.awt.*;
+import java.util.concurrent.*;
 
 public class P1egeoffroyOlympicRings extends JComponent {
+	//Prepare: Declare integers R, G, B. 
+	//Input: no input
+	//Process: Assign random integers to R, G, and B. Redefine R, G, and B for every new ring and for the title. Assign R, G, and B to new randomColor.  
+	//Output: print out the five Olympic Rings, which change colors every time the program runs, and a title. 
+	
+	
 	public void paintComponent(Graphics g)
 	   {
 	     Graphics2D g2 = (Graphics2D) g;
-
-		 g.drawOval (1,0,40,40);
-		 g.setColor(Color.YELLOW);
-
-		 g.drawOval (21,20,40,40);
-		 g.setColor(Color.BLACK);
-
-		 g.drawOval (41,0,40,40);
-		 g.setColor(Color.GREEN);
-
-		 g.drawOval (61,20,40,40);
-		 g.setColor(Color.RED);
-
-		 g.drawOval (81,0,40,40);
-		 g.setColor(Color.BLUE);
+	     
+	     int R, G, B;
+	     
+		 R = (int)(Math.random()*256);
+		 G = (int)(Math.random()*256);
+		 B = (int)(Math.random()*256);
+		 Color randomColor = new Color(R, G, B); 
+		 g.setColor(randomColor);
+		 g.drawOval (1,0,50,50);	
+	 
+		 R = (int)(Math.random()*256);
+		 G = (int)(Math.random()*256);
+		 B = (int)(Math.random()*256);
+		 Color randomColor1 = new Color(R, G, B); 
+		 g.setColor(randomColor1);
+		 g.drawOval (25,30,50,50); 
+		 
+		 R = (int)(Math.random()*256);
+		 G = (int)(Math.random()*256);
+		 B = (int)(Math.random()*256);
+		 Color randomColor2 = new Color(R, G, B); 
+		 g.setColor(randomColor2);
+		 g.drawOval (51,0,50,50);
+		 
+		 R = (int)(Math.random()*256);
+		 G = (int)(Math.random()*256);
+		 B = (int)(Math.random()*256);
+		 Color randomColor3 = new Color(R, G, B); 
+		 g.setColor(randomColor3);
+		 g.drawOval (75,30,50,50);
+		 
+		 R = (int)(Math.random()*256);
+		 G = (int)(Math.random()*256);
+		 B = (int)(Math.random()*256);
+		 Color randomColor4 = new Color(R, G, B); 
+		 g.setColor(randomColor4);
+		 g.drawOval (101,0,50,50);
+		 
+		 R = (int)(Math.random()*256);
+		 G = (int)(Math.random()*256);
+		 B = (int)(Math.random()*256); 
+		 Color randomColor5 = new Color(R, G, B); 
+		 g.setColor(randomColor5);
+		g.drawString("2018 OLYMPIC WINTER GAMES:", 0, 100); 
+		g.drawString("PyeongChang", 45, 110);
+		
+		
 	   }
-
+	
+	
 }
